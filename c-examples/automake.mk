cd7b5357b78d0b06fd89fa55a51285351a7a099f
Aaron Conole <aconole@bytheb.org>
2016-01-31 22:19:01 -0500

    BROKEN: don't touch this

---
+++ c-examples/automake.mk
@@ -0,0 +1,10 @@
+check_PROGRAMS += c-examples/simple/genber \
+	c-examples/simple/minbuf-ex \
+	c-examples/simple/expbuf-ex \
+	c-examples/simple/sbuf-ex \
+	c-examples/vdatestC 
+
+c_examples_simple_genber_SOURCES = \
+	asn1specs/p-rec.asn1 \
+	c-examples/simple/genber.c 
+
