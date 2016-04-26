c0a69e0eb59893548393386aa339b32f00df1f01
Aaron Conole <aconole@redhat.com>
2016-04-26 13:22:45 -0400

    debian: Distribute debian files
    This commit modifies the dist to include the debian build rules. This
    will enable a single tarball to be used to build for debian.
    
    Signed-off-by: Aaron Conole <aconole@bytheb.org>

---
+++ debian/automake.mk
@@ -0,0 +1,12 @@
+EXTRA_DIST += \
+	debian/changelog \
+	debian/compat \
+	debian/control \
+	debian/docs \
+	debian/esnacc.dirs \
+	debian/esnacc.install \
+	debian/libesnacc-dev.dirs \
+	debian/libesnacc-dev.install \
+	debian/libesnacc180.dirs \
+	debian/libesnacc180.install \
+	debian/rules
