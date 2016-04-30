f81c028eb376dd1d48b445baa9cc68b57511c3c4
Aaron Conole <aconole@bytheb.org>
2016-04-30 13:00:58 -0400

    c-examples/simple/minbuf: Add the minbuf example
    minbuf has been disabled since snacc 1.3+; however the minbuf facility is
    a useful thing to show a low-overhead buffer using the genbuf indirection
    system. This comment re-enables the minbuf example, and executes minbuf
    code.
    
    Signed-off-by: Aaron Conole <aconole@bytheb.org>

--- c-examples/automake.mk
+++ c-examples/automake.mk
@@ -1,7 +1,8 @@
-TESTS = c-examples/simple/genber
+TESTS = c-examples/simple/genber \
+	c-examples/simple/minbuf
 
-check_PROGRAMS += c-examples/simple/genber 
-#c-examples/simple/minbuf-ex \
+check_PROGRAMS += c-examples/simple/genber \
+	c-examples/simple/minbuf
 #	c-examples/simple/expbuf-ex \
 #	c-examples/simple/sbuf-ex 
 
@@ -15,6 +16,18 @@ c_examples_simple_genber_CFLAGS = \
 	-I$(top_srcdir)/c-lib/inc
 
 c_examples_simple_genber_LDADD = \
+	c-lib/libcasn1.la
+
+c_examples_simple_minbuf_SOURCES = \
+	asn1specs/p-rec.asn1 \
+	c-examples/simple/minbuf-ex.c 
+
+c_examples_simple_minbuf_CFLAGS = \
+	-I$(top_srcdir)/asn1specs \
+	-I$(builddir)/asn1specs \
+	-I$(top_srcdir)/c-lib/inc
+
+c_examples_simple_minbuf_LDADD = \
 	c-lib/libcasn1.la
 
 CLEANFILES += asn1specs/p-rec.h asn1specs/p-rec.c pr.ber
