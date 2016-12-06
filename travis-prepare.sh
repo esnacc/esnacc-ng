36b1a9192ced43976b2c54a6ace1bf50c4bf1e78
Aaron Conole <aconole@bytheb.org>
2016-12-06 14:36:52 -0500

    travis: docbook installation on osx
    OSX builds should install the docbook libraries so that going to the
    internet isn't as painful (it would fail there instead of in the actual
    build for 'vague' reasons).
    
    Signed-off-by: Aaron Conole <aconole@bytheb.org>

--- travis-prepare.sh
+++ travis-prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    brew install docbook docbook-xsl docbook2x
     brew uninstall libtool && brew install libtool ||
             echo "Failed fixing libtool error"
 fi
