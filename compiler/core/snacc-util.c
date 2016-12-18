esnacc/esnacc-ng

commit 67b3312887d6077402ffe230f470fe1983821c52

compiler/core/snacc-util: Introduce automatic tag utils
In order to properly perform automatic tagging, it is required to have a
two-pass system to gather all of the used tags, and then fill the holes
using the new tags.  Not much interop testing is done with gap filling.

Signed-off-by: Aaron Conole <aconole@bytheb.org>
Reviewed-by: Sagara Wickramasekara <sagaraw@gmail.com>

compiler/core/snacc-util.c
@@ -1622,5 +1622,76 @@ AppendSubtype PARAMS ((s, newSubtype, op),
 
 }  /* AppendSubtype */
 
+static int
+TagCodeFound(AsnList *tags, AsnInt startingTag)
+{
+    Tag *tag;
+    FOR_EACH_LIST_ELMT(tag, tags) {
+        if (tag->code == startingTag)
+            return 1;
+    }
+    return 0;
+}
 
+void
+AutomaticTagNamed PARAMS ((l, all),
+    NamedTypeList *l)
+{
+    AsnList *used_tag;
+    NamedType *e;
+    void *lcurr;
+    AsnInt startingTag = 0;
 
+    if (l == NULL) {
+        fprintf(errFileG, "AutomaticTagNamed - NULL tag list.\n");
+    }
+
+    /* Two passes */
+    used_tag = AsnListNew(sizeof(void*));
+    /* First pass, identify all the tags, and all the elements which
+       need tags */
+    lcurr = l->curr;
+    FOR_EACH_LIST_ELMT(e, l) {
+        if (e->type->tags != NULL) {
+            Tag *t;
+            void *curr = e->type->tags->curr;
+            FOR_EACH_LIST_ELMT(t, e->type->tags) {
+                if (t->tclass == CNTX) {
+                    APPEND(t, used_tag);
+                    break;
+                }
+            }
+            e->type->tags->curr = curr;
+        }
+    }
+    FOR_EACH_LIST_ELMT(e, l) {
+        int found = 0;
+        Tag *t;
+        FOR_EACH_LIST_ELMT(t, e->type->tags) {
+            if (t->tclass == CNTX) {
+                fprintf(errFileG, "CNTX tag exists\n");
+                found = 1;
+            }
+        }
+        if (!found) {
+            Tag *newTag = Malloc(sizeof(Tag));
+            newTag->tclass = CNTX;
+            newTag->form = PRIM;
+            newTag->valueRef = NULL;
+            /* It seems strange to only use context tagging; however, after
+             * some brief interop testing, that seems like the right thing to
+             * do */
+            if (!(e->type->tags))
+                AsnListFree(e->type->tags);
+            e->type->tags = AsnListNew(sizeof(void *));
+            while(TagCodeFound(used_tag, startingTag))
+                startingTag++;
+            newTag->code = startingTag++;
+            newTag->explicit = FALSE;
+            PREPEND(newTag, e->type->tags);
+        }
+    }
+    l->curr = lcurr;
+
+    AsnListFree(used_tag);
+}

compiler/core/snacc-util.h
@@ -108,6 +108,8 @@ BasicValue * GetLastNamedNumberValue PROTO ((NamedNumberList *valueList));
 
 void SetupType PROTO ((Type **t, enum BasicTypeChoiceId typeId, unsigned long lineNum));
 
+void AutomaticTagNamed PROTO ((NamedTypeList *l));
+
 void SetupMacroType PROTO ((Type **t, enum MacroTypeChoiceId macroTypeId, unsigned long lineNum));
 
 void SetupValue PROTO ((Value **v, enum BasicValueChoiceId valId, unsigned long lineNum));

commit 28aa572e81a248a82f67958ff8b9a26511efb31f

compiler: Fix automatic tags null dereference
Since the tag-list can be null, return here.  That will prevent a crash
further in the code.

Fixes: 67b3312887d6 ("compiler/core/snacc-util: Introduce automatic tag utils")
Signed-off-by: Aaron Conole <aconole@bytheb.org>
Acked-by: Sagara Wickramasekara <sagaraw@gmail.com>

compiler/core/snacc-util.c
@@ -1644,6 +1644,7 @@ AutomaticTagNamed PARAMS ((l, all),
 
     if (l == NULL) {
         fprintf(errFileG, "AutomaticTagNamed - NULL tag list.\n");
+        return;
     }
 
     /* Two passes */