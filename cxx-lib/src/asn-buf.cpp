esnacc/esnacc-ng

commit ea172d558054e77903f80817dffd946fb05e5c5a

cxx-lib/src/asn-buf: make GetSeg non-throwing.
GetSeg now returns either an empty buffer or NULL on error.

Signed-off-by: Aaron Conole <aconole@redhat.com>

cxx-lib/src/asn-buf.cpp
@@ -181,10 +181,17 @@ void AsnBuf::PutFileSeg(AsnFileSeg *pFs)
 
 char * AsnBuf::GetSeg(long segLen) const
 {
-   char *seg = new char[segLen];
-   // XXXX Make this a return version
-   GetSeg(seg, segLen);
-   return seg;
+    char *seg = new (std::nothrow) char[segLen];
+    if (!seg)
+        return NULL;
+
+    try {
+        GetSeg(seg, segLen);
+    } catch (BufferException) {
+        delete [] seg;
+        seg = NULL;
+    }
+    return seg;
 }
 
 
@@ -360,25 +367,20 @@ char AsnBuf::GetByte() const
 
    std::streambuf::int_type ch;
 
-   if (m_card != m_deck.end())
-   {
+   if (m_card != m_deck.end()) {
 #ifdef _DEBUG
-    Card *tmpCard = *m_card;
+       Card *tmpCard = *m_card;
 #endif
-	 while ((ch = (*m_card)->rdbuf()->sbumpc()) == EOF)
-    {
-         m_card++;
+       while ((ch = (*m_card)->rdbuf()->sbumpc()) == EOF)
+       {
+           m_card++;
 
-
-         if ( (m_card == m_deck.end()))
-         {
-              throw BufferException("Read past end of data", STACK_ENTRY);
-         }
-
-      }
-   }
-   else
-      throw BufferException("Read past end of data", STACK_ENTRY);
+           if ((m_card == m_deck.end())) {
+               throw BufferException("Read past end of data", STACK_ENTRY);
+           }
+       }
+   } else
+       throw BufferException("Read past end of data", STACK_ENTRY);
 
    return (char)ch;
 }

commit f03fb767a2023a14054fd03ca648a9560f862ca5

cxx-lib/asn-buf: fix a large parameter pass-by-value
The BufferException object is quite large (>512 bytes), which incurrs
a performance impact.  Convert the pass-by-value to a
pass-by-reference.

Fixes: ea172d558054 ("cxx-lib/src/asn-buf: make GetSeg non-throwing.")
Signed-off-by: Aaron Conole <aconole@bytheb.org>

cxx-lib/src/asn-buf.cpp
@@ -208,7 +208,7 @@ char * AsnBuf::GetSeg(long segLen) const
 
     try {
         GetSeg(seg, segLen);
-    } catch (BufferException) {
+    } catch (BufferException &) {
         delete [] seg;
         seg = NULL;
     }